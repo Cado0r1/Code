1682a81d9014cceceb10d3756f629c581f8eaa0e
Cado0r1 <126866792+Cado0r1@users.noreply.github.com>
2023-06-06 11:45:26 +0100

    Add files via upload

---
+++ JS TicTacToe/TicTacToe.js
@@ -0,0 +1,15 @@
+turn = 0
+
+function pressed(id){
+    if (document.getElementById(id).innerHTML == ''){
+        if (turn % 2 == 0){
+            document.getElementById(id).innerHTML = 'X'
+            document.getElementById('turn_num').innerHTML = 'Player Turn: O'
+        }
+        else{
+            document.getElementById(id).innerHTML = 'O'
+            document.getElementById('turn_num').innerHTML = 'Player Turn: X'
+        }
+        turn +=1
+    }
+}
